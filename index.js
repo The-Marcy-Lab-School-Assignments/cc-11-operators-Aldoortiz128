9ec4b5fb5afe2a0bf6461692c925c342b1d97608
aldoortiz128 <aldoortiz128@gmail.com>
2021-11-02 14:22:19 +0000

    pushed changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //1
 function areBothSame(x, y){
-  if (x == y) {return true;}
+  if (x == y) {return 'same';}
   else {return false;}
 }
 
@@ -22,22 +22,23 @@ else {return false;}
 
 //4
 function areAllFourSame(a, b, c, d){
-    if (a === b && b === c && c === d) {return true;
+    if (a === b && b === c && c === d) {return 'same';
 } else {return false;}
 }
 
 //5
 function areAllFourSameOrEqual(a, b, c, d){
-    if (a === b && b === c && c === d) {return 'equal';}
- else if ( a == b && b == c && c == d) {return 'same';}
+    if (a == b && b == c && c == d) {return 'same';}
+ else if ( a === b && b === c && c === d) {return 'equal';}
   else {return null;}
 }
 
 
-console.log (areBothSame(2, 4));
-console.log (areBothEqual(2, 6));
+console.log (areBothSame(2, '2'));
+console.log (areBothEqual(2, '2'));
 console.log (areAllFourEqual(-4, 'four', 'ninety', 9));
 console.log (areAllFourSame(4, '4', 'four', 4));
-console.log (areAllFourSameOrEqual(4, '4', 'four', 4));
+console.log (areAllFourSameOrEqual('4', '4', '4', '4')); 
+
 
 
